6c4d19b9c57671a241a3efa6878fd84d18f3b70b
suvanshenoy <suvanshenoy155@gmail.com>
2025-10-22 15:46:39 +0530

    fix: resolve merge conflicts

--- packages/ghost_vault_app/lib/main.dart
+++ packages/ghost_vault_app/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-=======
 import 'package:ghost_vault_app/screens/home_screen.dart';
->>>>>>> ff8ed40 (chore: add the initial structure of ghost_vault)
 
 void main() {
   runApp(const GhostVaultApp());
@@ -12,16 +9,11 @@ class GhostVaultApp extends StatelessWidget {
   const GhostVaultApp({super.key});
 
   @override
-<<<<<<< HEAD
-  Widget build(BuildContext build) {
-    return MaterialApp();
-=======
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/home',
       routes: {'/home': (context) => HomeScreen()},
     );
->>>>>>> ff8ed40 (chore: add the initial structure of ghost_vault)
   }
 }
