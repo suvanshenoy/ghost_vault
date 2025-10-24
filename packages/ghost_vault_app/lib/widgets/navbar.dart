6c0283815af2da7e95539b2ee431e097d3d3755d
suvanshenoy <suvanshenoy155@gmail.com>
2025-10-24 19:22:42 +0530

    feat(widgets): add bottom navigationbar

--- packages/ghost_vault_app/lib/widgets/navbar.dart
+++ packages/ghost_vault_app/lib/widgets/navbar.dart
@@ -1,43 +1,67 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ghost_vault_app/constants/themes.dart';
 
 class Navbar extends StatefulWidget {
-  const Navbar({super.key});
+  final List<Widget> screenWidgets;
+  const Navbar({super.key, required this.screenWidgets});
 
   @override
   State<Navbar> createState() => _NavbarState();
 }
 
 class _NavbarState extends State<Navbar> {
-  int _currentPageIndex = 0;
+  int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: NavigationBar(
+        backgroundColor: Themes.color1,
         onDestinationSelected: (int index) {
           setState(() {
-            _currentPageIndex = index;
+            currentPageIndex = index;
           });
         },
         indicatorColor: Colors.blue,
-        selectedIndex: _currentPageIndex,
+        selectedIndex: currentPageIndex,
         destinations: const <NavigationDestination>[
           NavigationDestination(
-            icon: Badge(isLabelVisible: false, child: Icon(Icons.home)),
+            icon: Badge(
+              isLabelVisible: false,
+              child: Icon(CupertinoIcons.home),
+            ),
             label: 'Home',
           ),
           NavigationDestination(
-            icon: Badge(isLabelVisible: false, child: Icon(Icons.lock)),
+            icon: Badge(
+              isLabelVisible: false,
+              child: Icon(CupertinoIcons.lock),
+            ),
             label: 'Vault',
           ),
           NavigationDestination(
             icon: Badge(
               isLabelVisible: false,
-              child: Icon(Icons.notification_important),
+              child: Icon(CupertinoIcons.bell),
             ),
             label: 'Notifications',
           ),
         ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            if (currentPageIndex == 0) widget.screenWidgets.first,
+            if (currentPageIndex == 1)
+              widget.screenWidgets[widget.screenWidgets.indexOf(
+                    widget.screenWidgets.first,
+                  ) +
+                  1],
+            if (currentPageIndex == 2) widget.screenWidgets.last,
+          ],
+        ),
       ),
     );
   }
