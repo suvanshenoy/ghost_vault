087f0211ba1a6537b227154d2fef27699bf792a3
suvanshenoy <suvanshenoy155@gmail.com>
2025-10-24 19:20:35 +0530

    chore(constants): create themes.dart to store colors

---
+++ packages/ghost_vault_app/lib/constants/themes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Themes {
+  static final Color color1 = Color(0XFF020617);
+  static final Color color2 = Color(0XFF161a29);
+  static final Color color3 = Color(0XFFf0f1f2);
+}
